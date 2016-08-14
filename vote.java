5c648fdf3a7fa814a1c023f55fb302e4782aef0f
himanshu <himanshusharmahs974@gmail.com>
2016-08-14 16:57:08 +0530

    eligible for voting

---
+++ vote.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class vote{
+public static void main(String args[]){
+int age=24;
+if (age>18){
+System.out.println("you are eligible to vote");
+}
+else{
+System.out.println("SORRY! you are not eligible to vote");
+}}}
