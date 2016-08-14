1b143691ae954367fb67e39d0393af04deb6765e
himanshu <himanshusharmahs974@gmail.com>
2016-08-14 17:02:59 +0530

    number of days in month

---
+++ numberofdays.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class numberofdays{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number of month you want to get days of");
+int a=sc.nextInt();
+if (a==1){
+System.out.println("you days are 31");
+}
+else if (a==2){
+System.out.println("you days are 29");
+}
+else if (a==3){
+System.out.println("you days are 31");
+}
+else if (a==4){
+System.out.println("you days are 30");
+}
+else if (a==5){
+System.out.println("you days are 31");
+}
+else if (a==6){
+System.out.println("you days are 30");
+}
+else if (a==7){
+System.out.println("you days are 31");
+}
+else if (a==8){
+System.out.println("you days are 31");
+}
+else if (a==9){
+System.out.println("you days are 30");
+}
+else if (a==10){
+System.out.println("you days are 31");
+}
+else if (a==11){
+System.out.println("you days are 30");
+}
+else if (a==12){
+System.out.println("you days are 31");
+}
+}}
